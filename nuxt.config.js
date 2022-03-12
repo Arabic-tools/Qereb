516f8f04d38a3042b4b1c5b21c29506a3834f4d2
romell491 <ulgasan491@yahoo.com>
2022-03-12 08:34:11 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,14 +25,14 @@ export default {
     ]
   },
 
+  
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "~assets/global.css",
 ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/hotjar.js'
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
